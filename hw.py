2e02adb6f5b2f9972af611ff3bfa3598bc9ec4be
aahanagoyal <neha108425@gmail.com>
2026-01-14 13:03:38 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen().bgcolor("powderblue")
+turtle.Screen().setup(500, 500)
+turtle.Screen().title("Turtle")
+turt=turtle.Turtle()
+
+
+for i in range(4):
+    turt.forward(150)
+    turt.left(90)
+
+turtle.done()
